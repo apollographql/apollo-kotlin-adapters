3d91e3fa58763b11dfaa1d572b1578f8064c8222
Martin Bonnin <martin@mbonnin.net>
2024-07-31 15:28:31 +0200

    update librarian

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import com.gradleup.librarian.gradle.librarianRoot
 
 plugins {
   id("org.jetbrains.kotlin.jvm").version("2.0.0").apply(false)
-  id("com.gradleup.librarian").version("0.0.4").apply(false)
+  id("com.gradleup.librarian").version("0.0.6").apply(false)
 }
 
 librarianRoot()

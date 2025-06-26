5f1405ea1ff181bd66dc6f33c40ac1579634aefa
Yang <ychescale9@gmail.com>
2025-06-26 13:32:13 +1000

    revert to `org.jetbrains.kotlin.jvm`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.gradleup.librarian.gradle.Librarian
 
 plugins {
-  id("org.jetbrains.kotlin.multiplatform").version("2.1.20").apply(false)
+  id("org.jetbrains.kotlin.jvm").version("2.1.20").apply(false)
   id("com.gradleup.librarian").version("0.0.7").apply(false)
   id("org.jetbrains.kotlinx.binary-compatibility-validator").version("0.18.0").apply(false)
 }

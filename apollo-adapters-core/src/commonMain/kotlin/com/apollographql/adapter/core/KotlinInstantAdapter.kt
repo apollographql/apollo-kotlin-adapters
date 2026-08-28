deb19a91695da87b49ea1cffe5c23dcd011855d5
Martin Bonnin <martin@mbonnin.net>
2026-08-28 14:14:54 +0200

    Bump target Kotlin version to 2.3

--- apollo-adapters-core/src/commonMain/kotlin/com/apollographql/adapter/core/KotlinInstantAdapter.kt
+++ apollo-adapters-core/src/commonMain/kotlin/com/apollographql/adapter/core/KotlinInstantAdapter.kt
@@ -4,7 +4,6 @@ import com.apollographql.apollo.api.Adapter
 import com.apollographql.apollo.api.CustomScalarAdapters
 import com.apollographql.apollo.api.json.JsonReader
 import com.apollographql.apollo.api.json.JsonWriter
-import kotlin.time.ExperimentalTime
 import kotlin.time.Instant
 
 /**
@@ -13,7 +12,6 @@ import kotlin.time.Instant
  *
  * It requires Android Gradle plugin 4.0 or newer and [core library desugaring](https://developer.android.com/studio/write/java8-support#library-desugaring).
  */
-@ExperimentalTime
 object KotlinInstantAdapter : Adapter<Instant> {
   override fun fromJson(reader: JsonReader, customScalarAdapters: CustomScalarAdapters): Instant {
     return Instant.parse(reader.nextString()!!)
